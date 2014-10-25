b474088cd6f46b360f8ed6bf4eeeb53f7274e092
Pablo Dorado <pandres95@boolinc.co>
2014-10-25 16:51:42 -0500

    Change models definition for Client. Added model for Period

--- models/Period.js
+++ models/Period.js
@@ -3,25 +3,25 @@ module.exports = function(app, sql){
     self = {
 
         find: function(values){
-            return sql.selectOne('client', values);
+            return sql.selectOne('period', values);
         },
 
         findAll: function(values){
-            return sql.select('client', values);
+            return sql.select('period', values);
         },
 
         create: function(object){
-            return sql.insert('client', object).then(function(){
+            return sql.insert('period', object).then(function(){
                 return self.find({id: object.id});
             });
         },
 
         update: function(id, object){
-            return sql.update('client', object, { id: id });
+            return sql.update('period', object, { id: id });
         },
 
         destroy: function(id){
-            return sql.delete('client', { id: id });
+            return sql.delete('period', { id: id });
         }
 
     };
