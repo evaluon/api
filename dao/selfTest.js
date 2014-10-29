573ce427e727d76abb3f337a9298e09ab000de38
Pablo Dorado <pandres95@boolinc.co>
2014-10-29 06:10:13 -0500

    Changed for checkFields util in SelfTest DAO

--- dao/selfTest.js
+++ dao/selfTest.js
@@ -1,60 +1,29 @@
 module.exports = function(app){
 
-    var _ = app.utils._,
+    var checkFields = app.utils.checkFields,
         SelfTest = app.db.SelfTest;
-
-    function checkFields(required, object){
-
-        missingFields = [];
-        for(field in required){
-
-            rField = required[field];
-            if(!(_.contains(object, rField) && object[rField])) {
-                missingFields.push(rField);
-            }
-
-        }
-        if(missingFields.length > 0){
-
-            throw {
-                message: "There are some missing fields",
-                missingFields: missingFields,
-                statusCode: 400
-            }
-            return false;
-
-        }
-        return true;
-
-    }
 
     return {
 
-        findAll: function(evaluee){
+        find: function(evaluee){
 
-            return checkFields(
-                [ 'user' ], { user: evaluee }
-            ).then(function(){
-                return SelfTest.findAll()
+            return checkFields([ 'user' ], { user: evaluee }).then(function(){
+                return SelfTest.find(evaluee);
             });
 
         },
 
-        find: function(evaluee){
+        findAll: function(evaluee){
 
-            return checkFields(
-                [ 'user' ], { user: evaluee }
-            ).then(function(){
-                return SelfTest.find(evaluee);
+            return checkFields([ 'user' ], { user: evaluee }).then(function(){
+                return SelfTest.findAll(evaluee);
             });
 
         },
 
         create: function(evaluee){
 
-            return checkFields(
-                [ 'user' ], { user: evaluee }
-            ).then(function(){
+            return checkFields([ 'user' ], { user: evaluee }).then(function(){
                 return SelfTest.create(evaluee);
             });
 
