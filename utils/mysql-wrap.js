217f5f5c5b10cba64afcf508289f25587b42ac71
Pablo Dorado <pandres95@boolinc.co>
2014-11-04 10:25:59 -0500

    Deleted log and issues on mysql-wrap.js module

--- utils/mysql-wrap.js
+++ utils/mysql-wrap.js
@@ -1,40 +1,5 @@
 var _ = require('underscore');
 var Q = require('q');
-var log = require('./log');
-
-/**
-* Setup a client to automatically replace itself if it is disconnected.
-*
-* @param {Connection} client
-*   A MySQL connection instance.
-*/
-function replaceClientOnDisconnect(client) {
-    client.on("error", function (err) {
-        if (!err.fatal) {
-            return;
-        }
-
-        if (err.code !== "PROTOCOL_CONNECTION_LOST") {
-            throw err;
-        }
-
-        // client.config is actually a ConnectionConfig instance, not the original
-        // configuration. For most situations this is fine, but if you are doing
-        // something more advanced with your connection configuration, then
-        // you should check carefully as to whether this is actually going to do
-        // what you think it should do.
-        client = mysql.createConnection(client.config);
-        replaceClientOnDisconnect(client);
-        connection.connect(function (error) {
-            if (error) {
-                // Well, we tried. The database has probably fallen over.
-                // That's fairly fatal for most applications, so we might as
-                // call it a day and go home.
-                process.exit(1);
-            }
-        });
-    });
-}
 
 var MySQLWrapError = function (error) {
 
