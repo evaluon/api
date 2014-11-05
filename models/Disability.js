b2c4b06a0c83d8aafe9e919592828d4b07e400d8
Pablo Dorado <pandres95@boolinc.co>
2014-11-05 06:15:01 -0500

    Complete evaluee statistic data for register

--- models/Disability.js
+++ models/Disability.js
@@ -3,7 +3,18 @@ module.exports = function(app, sql){
     return {
 
         findAll: function(){
-            return sql.select('disability');
+            var data = { disabilities: null, types: null, levels: null };
+
+            return sql.select('disability').then(function(disabilities){
+                data.disabilities = disabilities;
+                return sql.select('evaluee_type');
+            }).then(function(types){
+                data.types = types;
+                return sql.select('evaluee_level');
+            }).then(function(levels){
+                data.levels = levels;
+                return data;
+            });
         }
 
     }
