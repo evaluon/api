e7950462e56e59ab7e3c4d2c9d1a13d81f799e07
Pablo Dorado <pandres95@boolinc.co>
2014-11-05 11:26:46 -0500

    Log response row

--- models/Response.js
+++ models/Response.js
@@ -1,17 +1,21 @@
 module.exports = function(app, sql){
 
+    var log = app.utils.log;
+
     return {
 
         makeResponse: function(evaluee, test, question, answer){
-            return sql.insert(
-                'response',
-                {
-                    evaluee_id: evaluee,
-                    test_id: test,
-                    question_id: question,
-                    answer_id: answer
-                }
-            );
+
+            var row = {
+                evaluee_id: evaluee,
+                test_id: test,
+                question_id: question,
+                answer_id: answer
+            };
+
+            log.debug(row);
+
+            return sql.insert('response', row);
         }
 
     }
