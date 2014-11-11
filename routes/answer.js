217419c81a1ea38d7b65e249e492960e4ffdc2e6
Pablo Dorado <pandres95@boolinc.co>
2014-11-11 07:21:11 -0500

    Fix routes

--- routes/answer.js
+++ routes/answer.js
@@ -11,7 +11,7 @@ module.exports = function(app){
     },
     {
         method: 'put',
-        url: '/question/:id/:answer',
+        url: '/question/:id/answer/:answer',
         action: answer.addToQuestion,
         cors: true
     }
