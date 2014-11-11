29199fda7c762047c00cc21ce2542291227186fc
Pablo Dorado <pandres95@boolinc.co>
2014-11-11 09:44:24 -0500

    Close test non-updating

--- models/OpenedTest.js
+++ models/OpenedTest.js
@@ -6,19 +6,15 @@ module.exports = function(app, sql){
             return sql.insert(
                 'opened_test',
                 { evaluee_id: user, test_id: test, open_date: new Date }
-            ).then(function(result){
-
-            });
+            );
         },
 
         closeTest: function(user, test){
-            return sql.selectOne(
-                'opened_test', { evaluee_id: user, test_id: test }
-            ).then(function(test){
-                return sql.update(
-                    'opened_test', { close_date: new Date }, test
-                );
-            })
+            return sql.update(
+                'opened_test',
+                { close_date: new Date },
+                { evaluee_id: user, test_id: test }
+            );
         }
 
     }
