c8395e8267f87d3a3a5af681091ffa7779ebf7ac
Pablo Dorado <pandres95@boolinc.co>
2014-11-15 07:55:22 -0500

    Fix typo

--- models/Gamification.js
+++ models/Gamification.js
@@ -1,4 +1,4 @@
-modules.export = function(app, sql){
+module.exports = function(app, sql){
 
     var math = app.utils.math;
 
@@ -11,23 +11,24 @@ modules.export = function(app, sql){
 
         indicators: function(user){
 
-            return sql.selectOne('evaluee', { id: user }).then(function(isEvaluee){
+            return sql.selectOne(
+                'evaluee', { id: user }
+            ).then(function(isEvaluee){
                 if(!isEvaluee) throw {
                     message: "user_is_not_evaluee",
                     statusCode: 403
-                } else {
-                    return sql.one(
-                        "SELECT COUNT(q.*) AS rightQuestions " +
-                        "FROM " +
-                        "   question q, response r, answer a " +
-                        "WHERE " +
-                        "   a.id = r.answer_id AND a.right = TRUE AND " +
-                        "   q.id = r.question_id AND r.evaluee_id = ?",
-                        [ user ]
-                    )
                 }
+                return sql.one(
+                    "SELECT COUNT(q.*) AS rightQuestions " +
+                    "FROM " +
+                    "   question q, response r, answer a " +
+                    "WHERE " +
+                    "   a.id = r.answer_id AND a.right = TRUE AND " +
+                    "   q.id = r.question_id AND r.evaluee_id = ?",
+                    [ user ]
+                );
             }).then(function(data){
-                rightQuestions = data.rightQuestions;
+                var rightQuestions = data.rightQuestions;
 
                 return {
                     questions: rightQuestions,
@@ -35,10 +36,11 @@ modules.export = function(app, sql){
                     level: math.floor(math.log(rightQuestions, 15)),
                     levelName: function(){
                         var level = this.level,
-                            uBound = levels.length - 1;
+                        uBound = levels.length - 1;
                         return levels[level >= ubound ? ubound : level];
                     }
-                }
+                };
+
             });
 
         }
