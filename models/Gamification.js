9fbc02150f5cb2cf430de170d4e2eae56b01eb89
Pablo Dorado <pandres95@boolinc.co>
2014-11-15 07:03:33 -0500

    Added mathjs module

--- models/Gamification.js
+++ models/Gamification.js
@@ -2,6 +2,17 @@ modules.export = function(app, sql){
 
     return {
 
+        userIndicators: function(user){
+
+            return sql.selectOne('evaluee', { id: user }).then(function(isEvaluee){
+                if(!isEvaluee) throw {
+                    message: "user_is_not_evaluee",
+                    statusCode: 403
+                }
+            })
+
+        }
+
     }
 
 }
