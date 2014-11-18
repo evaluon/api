26e660d024c65044503e61628a476ac059ec5514
Pablo Dorado <pandres95@boolinc.co>
2014-11-18 07:46:04 -0500

    404 on no active periods

--- models/Period.js
+++ models/Period.js
@@ -11,9 +11,15 @@ module.exports = function(app, sql){
         findAll: function(institution_id){
             return sql.query(
                 'SELECT * FROM period WHERE institution_id = ? '
-                + 'AND stop_date >= ?'
+                + 'AND stop_date >= ? GROUP BY start_date ASC'
                 , [institution_id, new Date()]
-            );
+            ).then(function(periods){
+                if(periods.length == 0) throw {
+                    statusCode: 404,
+                    message: "no_active_period"
+                };
+                return periods;
+            });
         },
 
         create: function(object){
