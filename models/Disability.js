926290db194405f99031c0cf7433a0757a14bd13
Pablo Dorado <pandres95@boolinc.co>
2014-11-18 05:51:36 -0500

    Updated to include genders

--- models/Disability.js
+++ models/Disability.js
@@ -3,18 +3,23 @@ module.exports = function(app, sql){
     return {
 
         findAll: function(){
-            var data = { disabilities: null, types: null, levels: null };
+
+            var data = {}
 
             return sql.select('disability').then(function(disabilities){
                 data.disabilities = disabilities;
-                return sql.select('evaluee_type');
-            }).then(function(types){
-                data.types = types;
+                return sql.select('gender');
+            }).then(function(genders){
+                data.genders = genders;
                 return sql.select('evaluee_level');
             }).then(function(levels){
                 data.levels = levels;
+                return sql.select('evaluee_type');
+            }).then(function(types){
+                data.types = types;
                 return data;
             });
+
         }
 
     }
