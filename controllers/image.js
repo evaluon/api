472a17194b8caea0c2b2a18fcc88b917f0002d9e
Pablo Dorado <pandres95@boolinc.co>
2014-11-19 06:29:25 -0500

    Upload images

--- controllers/image.js
+++ controllers/image.js
@@ -7,8 +7,26 @@ module.exports = function(app){
     return {
 
         createImage: function(req, res, next){
-            log.debug(Object.keys(req));
-            responseView(req.files || false, res);
+
+            var body = {};
+
+            formidable(req).then(function(data){
+                body = data.fields;
+                var image = data.files.file;
+                return azure(
+                    app.config.azure, 'evaluon',
+                    image.path, _.last(image.name.split('.'))
+                );
+            }).then(function(data){
+                var location = data.result.blob;
+                return Dao.create({
+                    location: location,
+                    description: body.description
+                });
+            }).then(function(image){
+                responseView(image, res);
+            }).catch(next);
+
         }
     }
 
--- dao/image.js
+++ dao/image.js
@@ -1,40 +1,13 @@
 module.exports = function(app){
 
-    var _ = app.utils._,
-        log = app.utils.log,
-        checkFields = app.utils.checkFields,
-        Answer = app.db.Answer,
-        Question = app.db.Question,
+    var checkFields = app.utils.checkFields,
         Image = app.db.Image;
 
     return {
 
-        addQuestionImage: function(question_id, image){
+        create: function(image){
             return checkFields(
-                [':id', 'location', 'description'],
-                _.extend({ ':id': question_id }, image)
-            ).then(function(){
-                return Image.create(image);
-            }).then(function(image_id){
-                return Question.update(question_id, { image_id: image_id });
-            });
-        },
-
-        addAnswerImage: function(answer_id, image){
-            return checkFields(
-                [':id', 'location', 'description'],
-                _.extend({ ':id': question_id }, image)
-            ).then(function(){
-                return Image.create(image);
-            }).then(function(answer_id){
-                return Answer.update(answer_id, { image_id: image_id });
-            });
-        },
-
-        addImage: function(image){
-            return checkFields(
-                [':id', 'location', 'description'],
-                _.extend({ ':id': institution_id }, image)
+                ['location', 'description'], image
             ).then(function(){
                 return Image.create(image);
             });
