f9b94c42d36b334b8f5b98aa717e324a678d7117
Pablo Dorado <pandres95@boolinc.co>
2014-12-17 23:18:28 -0500

    Adding images to KAInTest

--- models/KAInTest.js
+++ models/KAInTest.js
@@ -1,14 +1,15 @@
 module.exports = function(app, sql){
 
     var _ = app.utils._,
-        log = app.utils.log;
+    log = app.utils.log;
 
     var self =  {
 
         addKAtoTest: function(test_id, knowledge_area){
             return sql.insert(
-                'test_knowledge_areas',
-                { test_id : test_id, knowledge_area_id : knowledge_area }
+                'test_knowledge_areas', {
+                    test_id : test_id, knowledge_area_id : knowledge_area
+                }
             );
         },
 
@@ -22,20 +23,43 @@ module.exports = function(app, sql){
                 "   tq.test_id = ? AND" +
                 "   q.id = tq.question_id"
                 , [test_id]
-            );
+            ).then(function(areas){
+                var qs = [];
+
+                for(area in areas){
+
+                    qs.push(
+                        (function(area) {
+                            return sql.selectOne(
+                                'image', { id: area.image_id }
+                            ).then(function(image){
+                                return _.extend({ image: image },
+                                    _.omit(area, 'image_id')
+                                );
+                            });
+                        })(areas[area])
+                    );
+                }
+
+                return Q.all(qs);
+            });
         },
 
         findKAinTest_evaluee: function(evaluee_id, test_id){
+
             return sql.query(
                 "SELECT 1 AS flag FROM opened_test WHERE evaluee_id = ? AND " +
                 "test_id = ? UNION SELECT 1 AS flag FROM self_test WHERE " +
                 "id = ? AND evaluee_id = ?"
                 , [ evaluee_id , test_id, test_id, evaluee_id ]
+
             ).then(function(ot){
+
                 if(!ot) throw {
                     message: 'test_unopened',
                     statusCode: 403
                 }
+
                 return sql.query(
                     "SELECT DISTINCT knowledge_area_id AS id " +
                     "FROM test_questions tq, question q " +
@@ -47,12 +71,34 @@ module.exports = function(app, sql){
                     "   ) AND" +
                     "   tq.test_id = ?"
                     , [test_id, evaluee_id, test_id]
-                );
-            })
+                ).then(function(areas){
+                    var qs = [];
+
+                    for(area in areas){
+
+                        qs.push(
+                            (function(area) {
+                                return sql.selectOne(
+                                    'image', { id: area.image_id }
+                                ).then(function(image){
+                                    return _.extend({ image: image },
+                                        _.omit(area, 'image_id')
+                                    );
+                                });
+                            })(areas[area])
+                        );
+
+                    }
+
+                    return Q.all(qs);
+
+                });
+
+            });
 
         }
 
-    }
+    };
 
     return self;
 
