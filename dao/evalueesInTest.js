aded8affd0f09bd5750b6250675607b67cd49330
Pablo Dorado <pandres95@boolinc.co>
2015-01-07 07:51:00 -0500

    Add feedback checked flag

--- dao/evalueesInTest.js
+++ dao/evalueesInTest.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
 
     var _ = app.utils._,
+        q = app.utils.q,
         log = app.utils.log,
         checkFields = app.utils.checkFields,
         Actors = app.db.Actors,
@@ -11,13 +12,14 @@ module.exports = function(app){
 
         evalueesInTest: function(group, test){
 
-            var openedTests = [], evalueeList = [];
+            var oTests = [], openedTests = [], evalueeList = [];
 
             return checkFields(
                 ['group_id', 'test_id'], { group_id: group, test_id: test }
             ).then(function(){
                 return OpenedTest.list(test);
             }).then(function(tests){
+                oTests = _.map(tests, _.clone);
                 openedTests = _.map(tests, function(t){ return t.evaluee_id });
                 return GroupEvaluee.groupEvaluees(group);
             }).then(function(evaluees){
@@ -31,6 +33,30 @@ module.exports = function(app){
                 return _.filter(evaluees, function(e) {
                     return _.contains(evalueeList, e.id)
                 });
+            }).then(function(evaluees){
+                var qs = [];
+
+                for(evaluee in evaluees){
+
+                    log.debug(oTests);
+                    qs.push(
+                        (function(evaluee){
+
+                            var test = _.findWhere(
+                                oTests, { evaluee_id: evaluee.id }
+                            );
+                            log.debug(test);
+
+                            evaluee.checked = (test.feedback != "");
+
+                            return evaluee;
+
+                        })(evaluees[evaluee])
+                    );
+
+                }
+
+                return q.all(qs);
             });
 
         }
